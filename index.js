cdb32d5711bdfb5489e5482047e27e9076b70777
bisma456 <bismatouseef2020@gmail.com>
2021-01-06 11:33:17 +0500

    done

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const PORT = 4000
+app.listen(PORT, (req,res)=>{
+console.log('server is running at', PORT)
+})
+app.use('/api/users', require('./routes/api/users'))
